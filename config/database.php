12ad5fefc3d89599bd888e5f2581be7e823f2299
iamnoobokay <60171702+iamnoobokay@users.noreply.github.com>
2021-02-18 16:33:09 +0545

    db done

--- config/database.php
+++ config/database.php
@@ -45,12 +45,12 @@ return [
 
         'mysql' => [
             'driver' => 'mysql',
-            'url' => env('DATABASE_URL'),
-            'host' => env('DB_HOST', '127.0.0.1'),
+            'url' => env(''),
+            'host' => env('DB_HOST', 'db4free.net'),
             'port' => env('DB_PORT', '3306'),
-            'database' => env('DB_DATABASE', 'forge'),
-            'username' => env('DB_USERNAME', 'forge'),
-            'password' => env('DB_PASSWORD', ''),
+            'database' => env('DB_DATABASE', 'employee_mgmt'),
+            'username' => env('DB_USERNAME', 'pranavpoudyal'),
+            'password' => env('DB_PASSWORD', 'independent100'),
             'unix_socket' => env('DB_SOCKET', ''),
             'charset' => 'utf8mb4',
             'collation' => 'utf8mb4_unicode_ci',
